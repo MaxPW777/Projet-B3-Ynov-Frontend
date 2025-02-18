5b4487062abd9a81cc65c0333d7408184d113549
Maximilian <mpinderwhite@gmail.com>
2025-02-18 19:35:45 +0100

    refactor: moved providers file + made children readonly

--- src/providers/QueryClientProvider.tsx
+++ src/providers/QueryClientProvider.tsx
@@ -2,7 +2,7 @@ import { QueryClient } from "@tanstack/query-core";
 import { QueryClientProvider } from "@tanstack/react-query";
 
 interface QueryClientProviderProps {
-  children: React.ReactNode;
+  readonly children: React.ReactNode;
 }
 
 export default function ReactQueryClientProvider({ children }: QueryClientProviderProps) {
